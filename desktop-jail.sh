91abe1e234144e48fab5147f685192a7a0f9754c
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-05-22 22:00:30 -0600

    Create desktop-jail.sh

---
+++ desktop-jail.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+# Build an iocage jail under TrueNAS 13.0 and set up a desktop environment for use with XRDP or VNC
+# git clone https://github.com/tschettervictor/truenas-iocage-desktop
+
+# Check for root privileges
+if ! [ $(id -u) = 0 ]; then
+   echo "This script must be run with root privileges"
+   exit 1
+fi
+
+#####
+#
+# General configuration
+#
+#####
+
+# Initialize defaults
+JAIL_IP=""
+JAIL_INTERFACES=""
+DEFAULT_GW_IP=""
+INTERFACE="vnet0"
+VNET="on"
+JAIL_NAME="desktopjail"
+CONFIG_NAME="desktopjail-config"
+VNC=""
+XRDP=""
+GNOME=""
+XFCE=""
+KDE=""
+MATE=""
+
+# Check for desktopjail-config and set configuration
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "${SCRIPT}")
+if ! [ -e "${SCRIPTPATH}"/"${CONFIG_NAME}" ]; then
+  echo "${SCRIPTPATH}/${CONFIG_NAME} must exist."
+  exit 1
+fi
+. "${SCRIPTPATH}"/"${CONFIG_NAME}"
+INCLUDES_PATH="${SCRIPTPATH}"/includes
+
+JAILS_MOUNT=$(zfs get -H -o value mountpoint $(iocage get -p)/iocage)
+RELEASE=$(freebsd-version | cut -d - -f -1)"-RELEASE"
+# If release is 13.1-RELEASE, change to 13.2-RELEASE
+if [ "${RELEASE}" = "13.1-RELEASE" ]; then
+  RELEASE="13.2-RELEASE"
+fi 
+
+#####
+#
+# Input/Config Sanity checks
+#
+#####
+
+# Check that necessary variables were set by uptimekuma-config
+if [ -z "${JAIL_IP}" ]; then
+  echo 'Configuration error: JAIL_IP must be set'
+  exit 1
+fi
+if [ -z "${JAIL_INTERFACES}" ]; then
+  echo 'JAIL_INTERFACES not set, defaulting to: vnet0:bridge0'
+JAIL_INTERFACES="vnet0:bridge0"
+fi
+if [ -z "${DEFAULT_GW_IP}" ]; then
+  echo 'Configuration error: DEFAULT_GW_IP must be set'
+  exit 1
+fi
+if [ -z "${POOL_PATH}" ]; then
+  echo 'Configuration error: POOL_PATH must be set'
+  exit 1
+fi
+
+# Extract IP and netmask, sanity check netmask
+IP=$(echo ${JAIL_IP} | cut -f1 -d/)
+NETMASK=$(echo ${JAIL_IP} | cut -f2 -d/)
+if [ "${NETMASK}" = "${IP}" ]
+then
+  NETMASK="24"
+fi
+if [ "${NETMASK}" -lt 8 ] || [ "${NETMASK}" -gt 30 ]
+then
+  NETMASK="24"
+fi
+
+#####
+#
+# Jail Creation
+#
+#####
+
+# Create the jail and install previously listed packages
+if ! iocage create --name "${JAIL_NAME}" -r "${RELEASE}" interfaces="${JAIL_INTERFACES}" ip4_addr="${INTERFACE}|${IP}/${NETMASK}" defaultrouter="${DEFAULT_GW_IP}" boot="on" host_hostname="${JAIL_NAME}" vnet="${VNET}"
+then
+	echo "Failed to create jail"
+	exit 1
+fi
+
+#####
+#
+# Directory Creation and Mounting
+#
+#####
+
+# Create and mount directories
+iocage exec "${JAIL_NAME}" mkdir -p /mnt/includes/
+iocage fstab -a "${JAIL_NAME}" "${INCLUDES_PATH}" /mnt/includes nullfs rw 0 0
+iocage exec "${JAIL_NAME}" mkdir -p /usr/local/etc/rc.d/
+
+# Don't need /mnt/includes any more, so unmount it
+iocage fstab -r "${JAIL_NAME}" "${INCLUDES_PATH}" /mnt/includes nullfs rw 0 0
+
+# Restart
+iocage restart "${JAIL_NAME}"
+
+echo "---------------"
+echo "Installation Complete!"
+echo "---------------"
